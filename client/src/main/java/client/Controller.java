1ccb6424e63d67329ad28978d737b03434a8e1b9
Witaliikaplun <vitalijkaplun@gmail.com>
2020-03-09 19:06:35 +0300

    Initial commit

---
+++ client/src/main/java/client/Controller.java
@@ -0,0 +1,315 @@
+package client;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    public TextArea textArea;
+    @FXML
+    public TextField textField;
+    @FXML
+    public HBox authPanel;
+    @FXML
+    public HBox msgPanel;
+    @FXML
+    public TextField loginField;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public ListView clientList;
+
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADDRESS = "localhost";
+    final int PORT = 8189;
+
+    private boolean authenticated;
+    private String nickname;
+
+    Stage regStage;
+    File file;
+    String fileName;
+
+    List<String> list;
+
+
+
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+        authPanel.setVisible(!authenticated);
+        authPanel.setManaged(!authenticated);
+        msgPanel.setVisible(authenticated);
+        msgPanel.setManaged(authenticated);
+        clientList.setVisible(authenticated);
+        clientList.setManaged(authenticated);
+
+
+
+        if (!authenticated) {
+            nickname = "";
+            writeFile(file);//запись в лог файл
+        }
+        textArea.clear();
+        setTitle("chat 2020");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        regStage = createRegWindow();
+        authenticated = false;
+        Platform.runLater(() -> {
+            Stage stage = (Stage) textField.getScene().getWindow();
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    System.out.println("bue");
+                    if (socket != null && !socket.isClosed()) {
+                        try {
+                            out.writeUTF("/end");
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        });
+
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    //цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/authok ")) {
+                            setAuthenticated(true);
+                            nickname = str.split(" ")[1];
+                            break;
+                        }
+                        textArea.appendText(str + "\n");
+                    }
+                    setTitle("chat 2020 : " + nickname);
+
+//После того как прошли аутентификацию и получили nickname--------------------
+                    String fileName = "history_" + nickname + ".txt";//формируем имя лог файла
+                    file = new File("history/" + fileName);//делаем ссылку на файл
+                    list = new ArrayList<>();//здесь будем хранить переписку текущего сеанса, для последующей загрузки в файл
+                    readFileRows(file, 5); //метод чтения из лог файла rows-это число строк которое необходимо
+
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                setAuthenticated(false);
+                                break;
+                            }
+                            if (str.startsWith("/clientlist ")) {
+                                String[] token = str.split(" ");
+                                Platform.runLater(() -> {
+                                    clientList.getItems().clear();
+                                    for (int i = 1; i < token.length; i++) {
+                                        clientList.getItems().add(token[i]);
+                                    }
+                                });
+                            }
+                            if (str.startsWith("/yournickis ")) {
+                                nickname = str.split(" ")[1];
+                                setTitle("chat 2020 : " + nickname);
+                            }
+
+                        } else {
+
+
+                            list.add(str + "\n");
+                            textArea.appendText(str + "\n");
+
+                        }
+                    }
+                } catch (SocketException e) {
+                    System.out.println("Сервер отключился");
+                    setAuthenticated(false);
+                } catch (IOException e) {
+//                    e.printStackTrace();
+                    System.out.println("Соединение с сервером разорвано");
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg() {
+        if (textField.getText().trim().length() > 0) {
+            try {
+                out.writeUTF(textField.getText());
+                textField.clear();
+                textField.requestFocus();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+//            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void setTitle(String title) {
+        Platform.runLater(() -> {
+            ((Stage) textField.getScene().getWindow()).setTitle(title);
+        });
+    }
+
+    public void clickClientList(MouseEvent mouseEvent) {
+//        System.out.println(clientList.getSelectionModel().getSelectedItem());
+        String receiver = clientList.getSelectionModel().getSelectedItem().toString();
+        textField.setText("/w " + receiver + " ");
+
+    }
+
+    public void registration(ActionEvent actionEvent) {
+        regStage.show();
+    }
+
+    private Stage createRegWindow() {
+        Stage stage = null;
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/reg.fxml"));
+            Parent root1 = fxmlLoader.load();
+            stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            RegController regController = fxmlLoader.getController();
+            regController.controller = this;
+
+            stage.setTitle("registration");
+            stage.setScene(new Scene(root1, 300, 200));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stage;
+    }
+
+    public void tryRegistr(String login, String password, String nickname) {
+        String msg = String.format("/reg %s %s %s", login, password, nickname);
+
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void writeFile(File file){
+        try(BufferedWriter out = new BufferedWriter(new FileWriter(file, true))) {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            //FileWriter out = new FileWriter(file, true);
+            out.append(outMy(list));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void readFile(File file){
+        if(file.exists()) {
+            try(BufferedReader in = new BufferedReader(new FileReader(file))) {
+                String str;
+                while ((str = in.readLine()) != null) {
+                    textArea.appendText(str + "\n");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public int readFileRows(File file, int rows){
+        List<String> list = new ArrayList<>();
+        int countRows = 0; //колличество строк прочитанных
+        if(file.exists()){
+            try(BufferedReader in = new BufferedReader(new FileReader(file))) {
+                String str;
+                while ((str = in.readLine()) != null) {
+                    list.add(str + "\n");
+                    countRows++;
+                }
+                for (int i = countRows - rows; i < countRows; i++) {
+                    textArea.appendText(list.get(i));
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return countRows;
+    }
+
+    public StringBuffer outMy(List<String> L){
+        StringBuffer s = new StringBuffer();
+        for (String l: L) {
+            s.append(l);
+        }
+        return s;
+    }
+}
